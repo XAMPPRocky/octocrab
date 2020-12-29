dc6c37959015034c22ad1a8ed2099f127dd2552c
aspen <aspenuwu@protonmail.com>
2020-12-29 16:00:43 -0500

    docs: Add documentation

--- src/api/repos/status.rs
+++ src/api/repos/status.rs
@@ -29,26 +29,33 @@ impl<'octo, 'r> CreateStatusBuilder<'octo, 'r> {
         }
     }
 
+    /// The SHA hash of the target commit.
     pub fn sha(mut self, sha: String) -> Self {
         self.sha = sha;
         self
     }
 
+    /// A string label to differentiate this status from the status of other systems.
     pub fn context(mut self, context: String) -> Self {
         self.context = Some(context);
         self
     }
 
+    /// A short description of the status.
     pub fn description(mut self, description: String) -> Self {
         self.description = Some(description);
         self
     }
 
+    /// The target URL to associate with this status. This URL will be linked from the GitHub UI to allow users to easily see the source of the status.  
+    /// For example, if your continuous integration system is posting build status, you would want to provide the deep link for the build output for this specific SHA:
+    /// http://ci.example.com/user/repo/build/sha
     pub fn target(mut self, target: String) -> Self {
         self.target_url = Some(target);
         self
     }
 
+    /// The state of the status.
     pub fn state(mut self, state: StatusState) -> Self {
         self.state = state;
         self
