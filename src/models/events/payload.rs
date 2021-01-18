34ae7190a93406d81b14f2a290a76af34210c674
Stephen O'Brien <wayofthepie@gmail.com>
2021-01-18 09:08:06 +0000

    Support IssuesEvent (#68)
    * Factor push and create events into modules
    
    * Update unknown event example
    
    * Support IssuesEvent event type

--- src/models/events/payload.rs
+++ src/models/events/payload.rs
@@ -1,4 +1,11 @@
+mod create;
+mod issues;
+mod push;
+
 use crate::models::repos::GitUser;
+pub use create::*;
+pub use issues::*;
+pub use push::*;
 use reqwest::Url;
 use serde::{Deserialize, Serialize};
 
@@ -13,32 +20,8 @@ use serde::{Deserialize, Serialize};
 pub enum EventPayload {
     PushEvent(PushEventPayload),
     CreateEvent(CreateEventPayload),
+    IssuesEvent(IssuesEventPayload),
     UnknownEvent(serde_json::Value),
-}
-
-/// The payload in a [`EventPayload::PushEvent`] type.
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[non_exhaustive]
-pub struct PushEventPayload {
-    pub push_id: u64,
-    pub size: u64,
-    pub distinct_size: u64,
-    pub r#ref: String,
-    pub head: String,
-    pub before: String,
-    pub commits: Vec<Commit>,
-}
-
-/// The payload in a [`EventPayload::CreateEvent`] type.
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[non_exhaustive]
-pub struct CreateEventPayload {
-    // a null ref will occur on the initial create event
-    pub r#ref: Option<String>,
-    pub ref_type: String,
-    pub master_branch: String,
-    pub description: Option<String>,
-    pub pusher_type: String,
 }
 
 /// A git commit in specific payload types.
