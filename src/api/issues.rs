88d258193bac1fbc38a3f1cafe5995dc5fe48d4d
Uchio Kondo <udzura@udzura.jp>
2021-03-03 14:34:55 +0900

    Add an entrypoint for whole repo's issue comments

--- src/api/issues.rs
+++ src/api/issues.rs
@@ -630,6 +630,23 @@ impl<'octo> IssueHandler<'octo> {
     pub fn list_comments(&self, issue_number: u64) -> ListCommentsBuilder<'_, '_> {
         ListCommentsBuilder::new(self, issue_number)
     }
+
+    /// Lists comments for issues in the whole repo.
+    /// ```no_run
+    /// # async fn run() -> octocrab::Result<()> {
+    /// let comment = octocrab::instance()
+    ///     .issues("owner", "repo")
+    ///     .list_issue_comments()
+    ///     .per_page(100)
+    ///     .page(2u32)
+    ///     .send()
+    ///     .await?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn list_issue_comments(&self) -> ListIssueCommentsBuilder<'_, '_> {
+        ListIssueCommentsBuilder::new(self)
+    }
 }
 
 #[derive(serde::Serialize)]
@@ -682,6 +699,49 @@ impl<'octo, 'r> ListCommentsBuilder<'octo, 'r> {
             owner = self.handler.owner,
             repo = self.handler.repo,
             issue = self.issue_number,
+        );
+
+        self.handler.crab.get(route, Some(&self)).await
+    }
+}
+
+#[derive(serde::Serialize)]
+pub struct ListIssueCommentsBuilder<'octo, 'r> {
+    #[serde(skip)]
+    handler: &'r IssueHandler<'octo>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    per_page: Option<u8>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    page: Option<u32>,
+}
+
+impl<'octo, 'r> ListIssueCommentsBuilder<'octo, 'r> {
+    pub(crate) fn new(handler: &'r IssueHandler<'octo>) -> Self {
+        Self {
+            handler,
+            per_page: None,
+            page: None,
+        }
+    }
+
+    /// Results per page (max 100).
+    pub fn per_page(mut self, per_page: impl Into<u8>) -> Self {
+        self.per_page = Some(per_page.into());
+        self
+    }
+
+    /// Page number of the results to fetch.
+    pub fn page(mut self, page: impl Into<u32>) -> Self {
+        self.page = Some(page.into());
+        self
+    }
+
+    /// Send the actual request.
+    pub async fn send(self) -> Result<crate::Page<models::issues::Comment>> {
+        let route = format!(
+            "repos/{owner}/{repo}/issues/comments",
+            owner = self.handler.owner,
+            repo = self.handler.repo,
         );
 
         self.handler.crab.get(route, Some(&self)).await
