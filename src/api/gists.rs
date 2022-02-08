b49c39e10ca25b6dd63b8c604cab222c38f2a51d
Matthias Wahl <matthiaswahl@m7w3.de>
2022-02-08 23:13:27 +0100

    Support Updating Gists via Patch

--- src/api/gists.rs
+++ src/api/gists.rs
@@ -36,6 +36,28 @@ impl<'octo> GistsHandler<'octo> {
         CreateGistBuilder::new(self.crab)
     }
 
+    /// Update an existing gist.
+    /// ```no_run
+    /// # async fn run() -> octocrab::Result<()> {
+    /// let gitignore = octocrab::instance()
+    ///   .gists()
+    ///   .update("aa5a315d61ae9438b18d")
+    ///   // Optional Parameters
+    ///   .description("Updated!")
+    ///   .file("hello_world.rs")
+    ///   .rename_to("fibonacci.rs")
+    ///   .with_content("fn main() {\n println!(\"I should be a Fibonacci!\");\n}")
+    ///   .file("delete_me.rs")
+    ///   .delete()
+    ///   .send()
+    ///   .await?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn update(&self, id: impl AsRef<str>) -> UpdateGistBuilder<'octo> {
+        UpdateGistBuilder::new(self.crab, format!("gists/{id}", id=id.as_ref()))
+    }
+
     /// Get a single gist.
     /// ```no_run
     /// # async fn run() -> octocrab::Result<()> {
@@ -101,4 +123,94 @@ struct CreateGistFile {
     #[serde(skip_serializing_if = "Option::is_none")]
     filename: Option<String>,
     content: String,
+}
+
+#[derive(Debug)]
+pub struct UpdateGistBuilder<'octo> {
+    crab: &'octo Octocrab,
+    gist_path: String,
+    data: UpdateGist
+}
+
+impl<'octo> UpdateGistBuilder<'octo> {
+    fn new(crab: &'octo Octocrab, gist_path: String) -> Self {
+        Self {
+            crab,
+            gist_path,
+            data: Default::default()
+        }
+    }
+   
+    pub fn description(mut self, description: impl Into<String>) -> Self {
+        self.data.description = Some(description.into());
+        self
+    }
+
+    pub fn file(self, filename: impl Into<String>) -> UpdateGistFileBuilder<'octo> {
+        UpdateGistFileBuilder::new(self, filename)
+    }
+    pub async fn send(self) -> Result<Gist> {
+        self.crab.patch(self.gist_path, Some(&self.data)).await
+    }
+}
+
+#[derive(Debug, Default, Serialize)]
+struct UpdateGist {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    files: Option<BTreeMap<String, Option<UpdateGistFile>>>
+}
+
+#[derive(Debug, Default, Serialize)]
+pub struct UpdateGistFile {
+    filename: Option<String>,
+    content: Option<String>
+}
+
+pub struct UpdateGistFileBuilder<'octo> {
+    builder: UpdateGistBuilder<'octo>,
+    filename: String,
+    file: Option<UpdateGistFile>
+}
+impl<'octo> UpdateGistFileBuilder<'octo> {
+    fn new(builder: UpdateGistBuilder<'octo>, filename: impl Into<String>) -> Self {
+        Self {
+            builder,
+            filename: filename.into(),
+            file: None
+        }
+    }
+
+    fn build(mut self) -> UpdateGistBuilder<'octo> {
+        self.builder.data.files.get_or_insert_with(BTreeMap::new).insert(self.filename, self.file);
+        self.builder
+    }
+
+    pub fn delete(mut self) -> UpdateGistBuilder<'octo> {
+        self.file = None;
+        self.build()
+    }
+
+    pub fn rename_to(mut self, filename: impl Into<String>) -> Self {
+        self.file.get_or_insert_with(Default::default).filename = Some(filename.into());
+        self
+    }
+
+    pub fn with_content(mut self, content: impl Into<String>) -> Self {
+        self.file.get_or_insert_with(Default::default).content = Some(content.into());
+        self
+    }
+
+    pub fn description(self, description: impl Into<String>) -> UpdateGistBuilder<'octo> {
+        self.build().description(description)
+    }
+
+    pub fn file(self, filename: impl Into<String>) -> UpdateGistFileBuilder<'octo> {
+        self.build().file(filename)
+    }
+
+    pub async fn send(self) -> Result<Gist> {
+        self.build().send().await
+    }
 }
