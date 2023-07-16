32946de3fff98585d96aaf465c307b880e3f26a9
Matthew Gapp <61894094+matthewgapp@users.noreply.github.com>
2023-07-15 19:04:23 -0700

    wip

--- src/page.rs
+++ src/page.rs
@@ -201,6 +201,7 @@ impl<T: serde::de::DeserializeOwned> crate::FromResponse for Page<T> {
                 "artifacts",
                 "repositories",
                 "installations",
+                "commits",
             ]
             .into_iter()
             .find(|v| json.get(v).is_some())
