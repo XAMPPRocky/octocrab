4115063e3d98546535fe1caf1deb6a0658fc5fcb
maflcko <6399679+maflcko@users.noreply.github.com>
2024-01-09 13:47:51 +0100

    models: Add CheckRunOutput (#515)
    Co-authored-by: MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>

--- src/models/checks.rs
+++ src/models/checks.rs
@@ -2,6 +2,16 @@ use super::*;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[non_exhaustive]
+pub struct CheckRunOutput {
+    pub title: Option<String>,
+    pub summary: Option<String>,
+    pub text: Option<String>,
+    pub annotations_count: u64,
+    pub annotations_url: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[non_exhaustive]
 pub struct CheckRun {
     pub id: CheckRunId,
     pub node_id: String,
@@ -10,6 +20,7 @@ pub struct CheckRun {
     pub url: String,
     pub html_url: Option<String>,
     pub conclusion: Option<String>,
+    pub output: CheckRunOutput,
     pub started_at: Option<chrono::DateTime<chrono::Utc>>,
     pub completed_at: Option<chrono::DateTime<chrono::Utc>>,
     pub name: String,
