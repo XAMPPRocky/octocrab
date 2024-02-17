7d5d3f83dea36d898161a7f0a2dcb0e9c63ffe01
Dmytro Horskyi <dmgorsky@gmail.com>
2024-02-18 01:04:09 +0200

    added /repos/{owner}/{repo}/check-suites/preferences

--- tests/check_suites.rs
+++ tests/check_suites.rs
@@ -4,7 +4,8 @@ use wiremock::{
 };
 
 use mock_error::setup_error_handler;
-use octocrab::models::checks::CheckSuite;
+use octocrab::models::checks::{AutoTriggerCheck, CheckSuite, CheckSuitePreferences};
+use octocrab::models::AppId;
 use octocrab::Octocrab;
 
 /// Unit test for calls to the `/repos/OWNER/REPO/contributors` endpoint
@@ -57,4 +58,40 @@ async fn should_create_check_suite() {
     let check_suite = result.unwrap();
 
     assert_eq!(check_suite.head_sha, head_sha);
+}
+
+#[tokio::test]
+async fn should_patch_check_suite_preferences() {
+    // mock infrastructure
+    let mock_server = MockServer::start().await;
+    let check_suite_response: CheckSuitePreferences =
+        serde_json::from_str(include_str!("resources/check_suite_preferences.json")).unwrap();
+    let response = ResponseTemplate::new(200).set_body_json(&check_suite_response);
+
+    let mock = Mock::given(method("PATCH"))
+        .and(path(format!(
+            "/repos/{OWNER}/{REPO}/check-suites/preferences"
+        )))
+        .respond_with(response.clone());
+    mock_server.register(mock).await;
+    let client = setup_octocrab(&mock_server.uri());
+    // fixture
+    let check_suite_patches = vec![AutoTriggerCheck {
+        app_id: AppId(42),
+        setting: true,
+    }];
+
+    let result = client
+        .checks(OWNER, REPO) // though, mocking here 'octocat' / 'Hello-World'
+        .update_preferences(check_suite_patches)
+        .send()
+        .await;
+
+    assert!(
+        result.is_ok(),
+        "expected successful result, got error: {:#?}",
+        result
+    );
+    let upd_pref_result = result.unwrap();
+    assert_eq!(upd_pref_result.preferences.auto_trigger_checks.len(), 2);
 }
