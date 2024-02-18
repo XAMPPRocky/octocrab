ee242bafa218faabbb15b6e4c5ea9d81a452014a
Dmytro Horskyi <dmgorsky@gmail.com>
2024-02-18 14:52:02 +0200

    added /repos/{owner}/{repo}/check-suites/{check_suite_id}

--- tests/check_suites.rs
+++ tests/check_suites.rs
@@ -4,8 +4,8 @@ use wiremock::{
 };
 
 use mock_error::setup_error_handler;
+use octocrab::models::{AppId, CheckSuiteId};
 use octocrab::models::checks::{AutoTriggerCheck, CheckSuite, CheckSuitePreferences};
-use octocrab::models::AppId;
 use octocrab::Octocrab;
 
 /// Unit test for calls to the `/repos/OWNER/REPO/contributors` endpoint
@@ -16,7 +16,7 @@ const REPO: &str = "octocrab";
 
 async fn setup_api(template: ResponseTemplate) -> MockServer {
     let mock_server = MockServer::start().await;
-
+    
     Mock::given(method("POST"))
         .and(path(format!("/repos/{OWNER}/{REPO}/check-suites")))
         .respond_with(template)
@@ -26,7 +26,7 @@ async fn setup_api(template: ResponseTemplate) -> MockServer {
         &mock_server,
         "POST on /repos/OWNER/REPO/check-suites not called",
     )
-    .await;
+        .await;
     mock_server
 }
 
@@ -41,22 +41,22 @@ async fn should_create_check_suite() {
     let template = ResponseTemplate::new(200).set_body_json(&check_suite_response);
     let mock_server = setup_api(template).await;
     let client = setup_octocrab(&mock_server.uri());
-
+    
     let head_sha = "d6fde92930d4715a2b49857d24b940956b26d2d3";
     let result = client
         .checks(OWNER, REPO)
         .create_check_suite(head_sha)
         .send()
         .await;
-
+    
     assert!(
         result.is_ok(),
         "expected successful result, got error: {:#?}",
         result
     );
-
+    
     let check_suite = result.unwrap();
-
+    
     assert_eq!(check_suite.head_sha, head_sha);
 }
 
@@ -67,7 +67,7 @@ async fn should_patch_check_suite_preferences() {
     let check_suite_response: CheckSuitePreferences =
         serde_json::from_str(include_str!("resources/check_suite_preferences.json")).unwrap();
     let response = ResponseTemplate::new(200).set_body_json(&check_suite_response);
-
+    
     let mock = Mock::given(method("PATCH"))
         .and(path(format!(
             "/repos/{OWNER}/{REPO}/check-suites/preferences"
@@ -80,13 +80,13 @@ async fn should_patch_check_suite_preferences() {
         app_id: AppId(42),
         setting: true,
     }];
-
+    
     let result = client
         .checks(OWNER, REPO) // though, mocking here 'octocat' / 'Hello-World'
         .update_preferences(check_suite_patches)
         .send()
         .await;
-
+    
     assert!(
         result.is_ok(),
         "expected successful result, got error: {:#?}",
@@ -95,3 +95,37 @@ async fn should_patch_check_suite_preferences() {
     let upd_pref_result = result.unwrap();
     assert_eq!(upd_pref_result.preferences.auto_trigger_checks.len(), 2);
 }
+
+#[tokio::test]
+async fn should_get_check_suite() {
+    // mock infrastructure
+    let mock_server = MockServer::start().await;
+    let check_suite_response: CheckSuite =
+        serde_json::from_str(include_str!("resources/check_suite.json")).unwrap();
+    let response = ResponseTemplate::new(200).set_body_json(&check_suite_response);
+    
+    const CHECK_SUITE_ID: i32 = 5;
+    let mock = Mock::given(method("GET"))
+        .and(path(format!(
+            "/repos/{OWNER}/{REPO}/check-suites/{CHECK_SUITE_ID}"
+        )))
+        .respond_with(response.clone());
+    mock_server.register(mock).await;
+    let client = setup_octocrab(&mock_server.uri());
+    
+    
+    let result = client
+        .checks(OWNER, REPO)
+        .get_check_suite(CheckSuiteId(5))
+        .send()
+        .await;
+    
+    assert!(
+        result.is_ok(),
+        "expected successful result, got error: {:#?}",
+        result
+    );
+    let get_cs_result = result.unwrap();
+    assert_eq!(get_cs_result.id, CheckSuiteId(5));
+}
+
