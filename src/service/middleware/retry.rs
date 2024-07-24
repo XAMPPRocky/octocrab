2a752c82efe2e6f75251d243f4ea38400e374ac6
Loïs Postula <lois@postu.la>
2024-07-24 18:49:58 +0200

    feat: allow sending non String payload with execute

--- src/service/middleware/retry.rs
+++ src/service/middleware/retry.rs
@@ -1,7 +1,8 @@
-use futures_util::future;
 use http::{Request, Response};
 use hyper_util::client::legacy::Error;
 use tower::retry::Policy;
+
+use crate::body::OctoBody;
 
 #[derive(Clone)]
 pub enum RetryConfig {
@@ -9,59 +10,24 @@ pub enum RetryConfig {
     Simple(usize),
 }
 
-impl<B> Policy<Request<String>, Response<B>, Error> for RetryConfig {
+impl<B> Policy<Request<OctoBody>, Response<B>, Error> for RetryConfig {
     type Future = futures_util::future::Ready<Self>;
 
     fn retry(
         &self,
-        _req: &Request<String>,
-        result: Result<&Response<B>, &Error>,
+        _req: &Request<OctoBody>,
+        _result: Result<&Response<B>, &Error>,
     ) -> Option<Self::Future> {
         match self {
             RetryConfig::None => None,
-            RetryConfig::Simple(count) => match result {
-                Ok(response) => {
-                    if response.status().is_server_error() || response.status() == 429 {
-                        if *count > 0 {
-                            Some(future::ready(RetryConfig::Simple(count - 1)))
-                        } else {
-                            None
-                        }
-                    } else {
-                        None
-                    }
-                }
-                Err(_) => {
-                    if *count > 0 {
-                        Some(future::ready(RetryConfig::Simple(count - 1)))
-                    } else {
-                        None
-                    }
-                }
-            },
+            RetryConfig::Simple(_count) => None,
         }
     }
 
-    fn clone_request(&self, req: &Request<String>) -> Option<Request<String>> {
+    fn clone_request(&self, _req: &Request<OctoBody>) -> Option<Request<OctoBody>> {
         match self {
             RetryConfig::None => None,
-            _ => {
-                // `Request` can't be cloned
-                let mut new_req = Request::builder()
-                    .uri(req.uri())
-                    .method(req.method())
-                    .version(req.version());
-                for (name, value) in req.headers() {
-                    new_req = new_req.header(name, value);
-                }
-
-                let body = req.body().clone();
-                let new_req = new_req.body(body).expect(
-                    "This should never panic, as we are cloning a components from existing request",
-                );
-
-                Some(new_req)
-            }
+            _ => None,
         }
     }
 }
