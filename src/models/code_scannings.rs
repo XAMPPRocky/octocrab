b641f1153fe3ead62e7c329847496d25c0c2b453
GeekMasher <geekmasher@gmail.com>
2024-08-08 12:14:41 +0100

    feat: Update Code Scanning models
    - Update Code Scanning webhooks with more data
    - Update Code Scanning models to follow the spec
    - Fix deserialization issues with `Environment` (static string, not an object in the spec)
    - Add CodeScanningState and DismissedReason enums

--- src/models/code_scannings.rs
+++ src/models/code_scannings.rs
@@ -3,21 +3,48 @@ use super::*;
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[non_exhaustive]
 pub struct CodeScanningAlert {
-    pub number: i64,
-    pub created_at: String,
-    pub updated_at: Option<String>,
-    pub url: String,
-    pub html_url: String,
-    pub state: String,
-    pub fixed_at: Option<String>,
-    pub dismissed_by: Dismisser,
-    pub dismissed_at: String,
-    pub dismissed_reason: String,
-    pub dismissed_comment: String,
+    /// The unique identifier of the code scanning alert.
+    pub number: u64,
+    pub created_at: chrono::DateTime<chrono::Utc>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
+    pub url: Url,
+    pub html_url: Url,
+    pub state: CodeScanningState,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub fixed_at: Option<chrono::DateTime<chrono::Utc>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub dismissed_by: Option<Dismisser>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub dismissed_at: Option<chrono::DateTime<chrono::Utc>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub dismissed_reason: Option<DismissedReason>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub dismissed_comment: Option<String>,
     pub rule: Rule,
     pub tool: Tool,
     pub most_recent_instance: MostRecentInstance,
-    pub instances_url: String,
+    pub instances_url: Url,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[non_exhaustive]
+#[serde(rename_all = "snake_case")]
+pub enum CodeScanningState {
+    Open,
+    Dismissed,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[non_exhaustive]
+#[serde(rename_all = "snake_case")]
+pub enum DismissedReason {
+    #[serde(rename = "false positive")]
+    FalsePositive,
+    #[serde(rename = "won't fix")]
+    WonTFix,
+    #[serde(rename = "used in tests")]
+    UsedInTests,
 }
 
 #[derive(Debug, Clone, Hash, Eq, PartialEq, Serialize, Deserialize)]
@@ -49,20 +76,28 @@ pub struct Dismisser {
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[non_exhaustive]
 pub struct Rule {
-    pub id: String,
-    pub severity: String,
-    pub description: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub id: Option<String>,
     pub name: String,
-    pub tags: Vec<String>,
-    pub security_severity_level: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub severity: Option<String>,
+    pub description: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub full_description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub tags: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub security_severity_level: Option<String>,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[non_exhaustive]
 pub struct Tool {
     pub name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub guid: Option<String>,
-    pub version: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub version: Option<String>,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -71,7 +106,7 @@ pub struct MostRecentInstance {
     #[serde(rename = "ref")]
     pub ref_field: String,
     pub analysis_key: String,
-    pub environment: Environment,
+    pub environment: String,
     pub category: String,
     pub state: String,
     pub commit_sha: String,
@@ -83,10 +118,13 @@ pub struct MostRecentInstance {
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[non_exhaustive]
 pub struct Environment {
-    #[serde(rename = "build-mode")]
+    #[serde(rename = "build-mode", skip_serializing_if = "Option::is_none")]
     pub build_mode: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub category: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub language: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub runner: Option<Vec<String>>,
 }
 
