8aa113e07965cf6c12859640c31c656471391ef0
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2024-08-30 02:13:24 -0700

    Add `DeviceCodes::poll_until_available` method (#679)

--- examples/device_flow.rs
+++ examples/device_flow.rs
@@ -1,6 +1,4 @@
-use either::Either;
 use http::header::ACCEPT;
-use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> octocrab::Result<()> {
@@ -17,28 +15,7 @@ async fn main() -> octocrab::Result<()> {
         "Go to {} and enter code {}",
         codes.verification_uri, codes.user_code
     );
-    let mut interval = Duration::from_secs(codes.interval);
-    let mut clock = tokio::time::interval(interval);
-    let auth = loop {
-        clock.tick().await;
-        match codes.poll_once(&crab, &client_id).await? {
-            Either::Left(auth) => break auth,
-            Either::Right(cont) => match cont {
-                octocrab::auth::Continue::SlowDown => {
-                    // We were request to slow down. We add five seconds to the polling
-                    // duration.
-                    interval += Duration::from_secs(5);
-                    clock = tokio::time::interval(interval);
-                    // The first tick happens instantly, so we tick that off immediately.
-                    clock.tick().await;
-                }
-                octocrab::auth::Continue::AuthorizationPending => {
-                    // The user has not clicked authorize yet, but nothing has gone wrong.
-                    // We keep polling.
-                }
-            },
-        }
-    };
+    let auth = codes.poll_until_available(&crab, &client_id).await?;
 
     println!("Authorization succeeded with access to {:?}", auth.scope);
     Ok(())
