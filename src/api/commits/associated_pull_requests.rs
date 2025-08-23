132496e3d3cc23832d9f595ecedf34fca2552d1b
Gautam Korlam <kageiit@users.noreply.github.com>
2025-08-23 01:18:51 -0700

    Pull in updates from upstream (#8)
    ## Description
    
    <!-- 
    Can leave blank if title is descriptive enough
    This PR [adds/removes/fixes/replaces] the [feature/bug/etc]. 
    -->
    
    ## Issues
    
    <!-- Uncomment to add Linears
    Fixes 
    -->
    
    ## Test Plan
    
    <!-- 
    Please do not leave this blank 
    [unit tests/staging/regression tests/integration tests/etc] 
    -->
    
    ## Revert Plan
    
    <!-- 
    Please do not leave this blank 
    [revert diff/Rollback flag name/delete file/etc] 
    -->
    
    ---------
    
    Co-authored-by: maflcko <6399679+maflcko@users.noreply.github.com>
    Co-authored-by: MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
    Co-authored-by: Eliot <robertseliot2011@gmail.com>

--- src/api/commits/associated_pull_requests.rs
+++ src/api/commits/associated_pull_requests.rs
@@ -13,8 +13,8 @@ pub enum PullRequestTarget {
 impl Display for PullRequestTarget {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Branch(branch) => write!(f, "{}", branch),
-            Self::Sha(commit) => write!(f, "{}", commit),
+            Self::Branch(branch) => write!(f, "{branch}"),
+            Self::Sha(commit) => write!(f, "{commit}"),
         }
     }
 }
