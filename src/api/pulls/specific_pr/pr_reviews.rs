3ab5029ed7597818f38f2b267287103467357000
Dmytro Horskyi <dmgorsky@gmail.com>
2025-09-29 17:23:47 +0300

    doccomment fix

--- src/api/pulls/specific_pr/pr_reviews.rs
+++ src/api/pulls/specific_pr/pr_reviews.rs
@@ -52,8 +52,8 @@ impl<'octo, 'b> ReviewsBuilder<'octo, 'b> {
     /// ```no_run
     /// # use octocrab::models::pulls::{ReviewAction, ReviewComment};
     ///
-    /// async fn run() -> octocrab::Result<()> {
-    ///  let octocrab = octocrab::Octocrab::default();
+    /// # async fn run() -> octocrab::Result<()> {
+    /// # let octocrab = octocrab::Octocrab::default();
     ///
     ///  let _ = octocrab.pulls("owner", "repo")
     ///  .pull_number(42)
@@ -62,11 +62,11 @@ impl<'octo, 'b> ReviewsBuilder<'octo, 'b> {
     ///     "ecdd80bb57125d7ba9641ffaa4d7d2c19d3f3091",
     ///     "This is close to perfect! Please address the suggested inline change.",
     ///     ReviewAction::RequestChanges,
-    ///     [ReviewComment{ path: "file.md", position: 6, body: "Please add more information" }]
+    ///     [ReviewComment{ path: "file.md", position: 6, body: "Please add more information".to_string() }]
     ///  )
     ///  .await?;
-    ///  Ok(())
-    /// }
+    ///  # Ok(())
+    /// # }
     /// ```
     pub async fn create_review(
         &self,
