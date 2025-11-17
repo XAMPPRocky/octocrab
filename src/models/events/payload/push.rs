a7343d2798ae07fcd75e541beb256d8f95361bab
Alyssa Ross <hi@alyssa.is>
2025-11-17 16:38:59 +0100

    Remove deleted fields from PushEventPayload
    These were removed from the GitHub API on 7 October 2025[1].  Since
    then, trying to load repository events using Octocrab will fail:
    
    	called `Result::unwrap()` on an `Err` value: Error("missing field `size`", line: 0, column: 0)
    
    Link: https://github.blog/changelog/2025-08-08-upcoming-changes-to-github-events-api-payloads/ [1]

--- src/models/events/payload/push.rs
+++ src/models/events/payload/push.rs
@@ -8,12 +8,9 @@ use crate::models::PushId;
 #[non_exhaustive]
 pub struct PushEventPayload {
     pub push_id: PushId,
-    pub size: u64,
-    pub distinct_size: u64,
     pub r#ref: String,
     pub head: String,
     pub before: String,
-    pub commits: Vec<Commit>,
 }
 
 #[cfg(test)]
@@ -33,28 +30,9 @@ mod test {
         match payload {
             EventPayload::PushEvent(payload) => {
                 assert_eq!(payload.push_id.0, 6080608029);
-                assert_eq!(payload.size, 1);
-                assert_eq!(payload.distinct_size, 1);
                 assert_eq!(payload.r#ref, "refs/heads/master");
                 assert_eq!(payload.head, "eb1a60c03544dcea290f2d57bb66ae188ce25778");
                 assert_eq!(payload.before, "9b2afb3a8e03fb30cc09e5efb64823bde802cf59");
-                assert_eq!(payload.commits.len(), 1);
-                let commit = payload.commits.first().unwrap();
-                assert_eq!(commit.sha, "eb1a60c03544dcea290f2d57bb66ae188ce25778");
-                assert_eq!(
-                    commit.author,
-                    CommitAuthor {
-                        name: "readme-bot".to_string(),
-                        email: Some("readme-bot@example.com".to_string()),
-                        date: None,
-                    }
-                );
-                assert_eq!(commit.message, "Charts Updated");
-                assert!(commit.distinct);
-                assert_eq!(
-                    commit.url,
-                    Url::parse("https://api.github.com/repos/user/user/commits/12345").unwrap()
-                );
             }
             _ => panic!("unexpected event deserialized"),
         }
