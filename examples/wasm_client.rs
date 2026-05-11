57f80d42c95954955ab1344e1bc0801b94b794a0
dairon2 <122072006+dairon2@users.noreply.github.com>
2026-05-11 13:36:26 -0500

    Fix wasm example cfg on stable Rust

--- examples/wasm_client.rs
+++ examples/wasm_client.rs
@@ -1,32 +1,37 @@
-cfg_select! {
-    target_arch = "wasm32" => {
-        use std::sync::Arc;
+#[cfg(target_arch = "wasm32")]
+mod wasm_client {
+    use std::sync::Arc;
 
-        use http::{header::USER_AGENT, Uri};
-        use octocrab::service::middleware::base_uri::BaseUriLayer;
-        use octocrab::service::middleware::extra_headers::ExtraHeadersLayer;
-        use octocrab::service::wasm::ReqwestService;
-        use octocrab::{AuthState, Octocrab, OctocrabBuilder};
+    use http::{header::USER_AGENT, Uri};
+    use octocrab::service::middleware::base_uri::BaseUriLayer;
+    use octocrab::service::middleware::extra_headers::ExtraHeadersLayer;
+    use octocrab::service::wasm::ReqwestService;
+    use octocrab::{AuthState, Octocrab, OctocrabBuilder};
 
-        fn build_octocrab() -> octocrab::Result<Octocrab, std::convert::Infallible> {
-            OctocrabBuilder::new_empty()
-                .with_service(ReqwestService::new())
-                .with_layer(&BaseUriLayer::new(Uri::from_static(
-                    "https://api.github.com",
-                )))
-                .with_layer(&ExtraHeadersLayer::new(Arc::new(vec![(
-                    USER_AGENT,
-                    "octocrab".parse().unwrap(),
-                )])))
-                .with_auth(AuthState::None)
-                .build()
-        }
-
-        fn main() {
-            let _octocrab = build_octocrab().expect("build wasm client");
-        }
+    fn build_octocrab() -> octocrab::Result<Octocrab, std::convert::Infallible> {
+        OctocrabBuilder::new_empty()
+            .with_service(ReqwestService::new())
+            .with_layer(&BaseUriLayer::new(Uri::from_static(
+                "https://api.github.com",
+            )))
+            .with_layer(&ExtraHeadersLayer::new(Arc::new(vec![(
+                USER_AGENT,
+                "octocrab".parse().unwrap(),
+            )])))
+            .with_auth(AuthState::None)
+            .build()
     }
-    _ => {
-        fn main() {}
+
+    pub fn run() {
+        let _octocrab = build_octocrab().expect("build wasm client");
     }
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    wasm_client::run();
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
 }
