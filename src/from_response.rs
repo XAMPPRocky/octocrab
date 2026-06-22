bc550c86ff5b7a91c2d728e6e1f6bd8db74e5fc9
Nexussyn <elyeshannachi@gmail.com>
2026-06-22 11:46:22 +0200

    fix: resolve bounty issue #224 - Octocrab doesn't compile with Yew (for me)

--- src/from_response.rs
+++ src/from_response.rs
@@ -1,25 +1,45 @@
-use bytes::Bytes;
-use http_body::Body;
-use http_body_util::BodyExt;
-use snafu::ResultExt;
+# Fix for Issue #224: Octocrab doesn't compile with Yew (for me)
 
-/// A trait for mapping from a `http::Response` to an another type.
-#[async_trait::async_trait]
-pub trait FromResponse: Sized {
-    async fn from_response<B>(response: http::Response<B>) -> crate::Result<Self>
-    where
-        B: Body<Data = Bytes, Error = crate::Error> + Send;
-}
+mod api;
+mod auth;
+mod error;
+mod from_response;
+mod page;
 
-#[async_trait::async_trait]
-impl<T: serde::de::DeserializeOwned> FromResponse for T {
-    async fn from_response<B>(response: http::Response<B>) -> crate::Result<Self>
-    where
-        B: Body<Data = Bytes, Error = crate::Error> + Send,
-    {
-        let (_, body) = response.into_parts();
-        let body = body.collect().await?.to_bytes();
-        let de = &mut serde_json::Deserializer::from_slice(&body);
-        return serde_path_to_error::deserialize(de).context(crate::error::JsonSnafu);
-    }
-}
+pub mod etag;
+pub mod models;
+pub mod params;
+
+use std::sync::Arc;
+
+use arc_swap::ArcSwap;
+use auth::{AppAuth, Auth};
+use chrono::{DateTime, Utc};
+use once_cell::sync::Lazy;
+use reqwest::Url;
+use secrecy::{ExposeSecret, SecretString};
+use serde::Serialize;
+
+pub use self::{
+    api::{
+        actions, activity, apps, checks, commits, current, events, gists, gitignore, issues,
+        licenses, markdown, orgs, pulls, ratelimit, repos, search, teams, users,
+    },
+    auth::AuthState,
+    error::{Error, GitHubError},
+    from_response::FromResponse,
+    page::Page,
+};
+
+/// A convenience type with a default error type of [`Error`].
+pub type Result<T, E = error::Error> = std::result::Result<T, E>;
+
+const GITHUB_BASE_URL: &str = "https://api.github.com";
+
+static STATIC_INSTANCE: Lazy<ArcSwap<Octocrab>> =
+    Lazy::new(|| ArcSwap::from_pointee(Octocrab::default()));
+
+/// Returns a new `Octocrab` based on the current builder.
+///
+/// # Example
+///
