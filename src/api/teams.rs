02c9d81ed81f75351f1ebe7c3e1df1c26664029f
Dmytro Horskyi <dmgorsky@gmail.com>
2026-09-07 11:48:37 +0300

    team memberships for #551

--- src/api/teams.rs
+++ src/api/teams.rs
@@ -6,6 +6,7 @@ mod edit;
 mod invitations;
 mod list;
 mod members;
+mod memberships;
 mod team_repos;
 
 pub use self::{
@@ -15,6 +16,7 @@ pub use self::{
     invitations::ListTeamInvitationsBuilder,
     list::ListTeamsBuilder,
     members::ListTeamMembersBuilder,
+    memberships::TeamMembershipBuilder,
     team_repos::{ListTeamRepositoriesBuilder, TeamRepoHandler},
 };
 use http::Uri;
@@ -198,5 +200,21 @@ impl<'octo> TeamHandler<'octo> {
     /// ```
     pub fn invitations(&self, team_slug: impl Into<String>) -> ListTeamInvitationsBuilder<'_, '_> {
         ListTeamInvitationsBuilder::new(self, team_slug.into())
+    }
+
+    /// Creates a new `TeamMembershipBuilder` for the specified team,
+    /// that allows you to manage this team's memberships.
+    /// ```no_run
+    /// # async fn run() -> octocrab::Result<()> {
+    /// let membership = octocrab::instance()
+    ///     .teams("owner")
+    ///     .memberships("team")
+    ///     .get("username")
+    ///     .await?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn memberships(&self, team_slug: impl Into<String>) -> TeamMembershipBuilder<'_> {
+        TeamMembershipBuilder::new(self.crab, self.owner.clone(), team_slug.into())
     }
 }
