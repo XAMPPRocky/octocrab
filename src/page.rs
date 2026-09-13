a23620837379dc740a7168836607d9e6c7dac46a
Dmytro Horskyi <dmgorsky@gmail.com>
2026-09-13 19:58:56 +0300

    Recognize seats field in paginated responses

--- src/page.rs
+++ src/page.rs
@@ -219,6 +219,7 @@ impl<T: serde::de::DeserializeOwned> crate::FromResponse for Page<T> {
                 "machines",
                 "seats",
                 "alerts",
+                "seats",
             ]
             .into_iter()
             .find(|v| json.get(v).is_some())
