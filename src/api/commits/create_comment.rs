1f365a7ffe1878b4e831ad89451fd99bea368b4d
Dmytro Horskyi <dmgorsky@gmail.com>
2026-09-16 00:20:49 +0300

    fixed CreateCommentBuilder serialization

--- src/api/commits/create_comment.rs
+++ src/api/commits/create_comment.rs
@@ -4,6 +4,7 @@ use super::*;
 pub struct CreateCommentBuilder<'octo, 'r> {
     #[serde(skip)]
     handler: &'r super::CommitHandler<'octo>,
+    #[serde(skip)]
     sha: String,
     body: String,
     #[serde(skip_serializing_if = "Option::is_none")]
